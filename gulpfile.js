29d7c2dd9da722c9be53e2cf98740b8be367a019
mluciacaicedo <mluciacaicedo@gmail.com>
2016-11-09 16:29:41 -0500

    Avance Taller

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+/*
+* Dependencias
+*/
+var gulp = require('gulp');
+var  concat = require('gulp-concat');
+var uglify = require('gulp-uglify'); 
+var minify = require('gulp-minify');
+var sourcemaps=require('gulp-sourcemaps');
+var cleanCSS=require('gulp-clean-css');
+
+
+/*
+* Configuración de la tarea 'demo'
+*/
+gulp.task('juntar', function () {
+  gulp.src(['bower_components/jquery/dist/jquery.js','bower_components/angular/angular.js','bower_components/bootstrap/dist/js/bootstrap.js'])
+  .pipe(concat('vendor.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('public/javascripts/'));
+});
+
+// 'bower_components/bootstrap/dist/css/bootstrap.css
+
+gulp.task('minify', function () {
+    gulp.src('bower_components/bootstrap/dist/css/bootstrap.css')
+        .pipe(minify({keepBreaks: true}))
+        .pipe(concat('vendor.min.css'))
+        .pipe(gulp.dest('public/stylesheets/'));
+});
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Taller 1 Angular JS' });
 });
 
 module.exports = router;
