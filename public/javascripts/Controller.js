8b815f8160ec8c50c7dc789afac592501e570488
mluciacaicedo <mluciacaicedo@gmail.com>
2016-11-11 17:38:16 -0500

    Avance Taller

---
+++ public/javascripts/Controller.js
@@ -0,0 +1,6 @@
+app.controller('OperacionesController', OperacionesController);
+
+  BooksController.$inject = ['$scope'];
+  function OperacionesController($scope){
+      
+  }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Taller 1 Angular JS' });
+  res.render('taller1', { title: 'Taller 1 Angular JS' });
 });
 
 module.exports = router;
