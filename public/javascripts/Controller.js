e32401806763ac1a2a185a44c48c5011b407c839
mluciacaicedo <mluciacaicedo@gmail.com>
2016-11-15 18:27:04 -0500

    Actualizacion

--- public/javascripts/Controller.js
+++ public/javascripts/Controller.js
@@ -1,6 +1,35 @@
-app.controller('OperacionesController', OperacionesController);
+var app= angular.module('taller1', []);
 
-  BooksController.$inject = ['$scope'];
-  function OperacionesController($scope){
-      
-  }
+app.controller('OperacionesController', function($scope) {
+  
+  $scope.sumar = function() { 
+     $scope.resultado = parseInt($scope.numUno) + parseInt($scope.numDos);
+  };
+  $scope.restar = function() { 
+      $scope.resultado = parseInt($scope.numUno) - parseInt($scope.numDos); 
+  };
+  $scope.multiplicar = function() { 
+      $scope.resultado = parseInt($scope.numUno) * parseInt($scope.numDos); 
+  };
+  $scope.dividir = function() { 
+      $scope.resultado = parseInt($scope.numUno) / parseInt($scope.numDos); 
+  };
+
+  $scope.clear = function() { 
+      $scope.resultado = ""; 
+      $scope.numUno = "";
+      $scope.numDos = "";
+  };
+});
+
+app.controller('TiendaController',TiendaController);
+  TiendaController.$inject = ['$scope'];
+    function TiendaController($scope){
+        $scope.articulo = articulo;
+    }
+
+app.controller('TiendaController1',TiendaController1);
+  TiendaController1.$inject = ['$scope'];
+    function TiendaController1($scope){
+        $scope.articulos = articulos;
+    }
---
+++ public/javascripts/app2.js
@@ -0,0 +1,27 @@
+var articulos = [{
+    nombre: 'FUJIFILM FinePix S8600 - negro - Cámara fotográfica digital',
+    precio: 149.00,
+    imagen: 'images/camara.jpg',
+    stock: 5
+}, {
+    nombre: 'PANASONIC VIERA TX-55AS650E - Televisor LED 3D Smart TV',
+    precio: 1499.00,
+    imagen: 'images/panasonic.jpg',
+    stock: 9
+}, {
+    nombre: 'SAMSUNG Galaxy S4 Value Edition - blanco - 16 GB - Smartphone',
+    precio: 399.00,
+    imagen: 'images/Galaxy.jpg',
+    stock: 22,
+}, {
+    nombre: 'WD WD Red WD40EFRX - 4 TB - Disco duro interno - 3.5"',
+    precio: 174.90,
+    imagen: 'images/dd.jpg',
+    stock: 0,
+}, {
+    nombre: 'SAMSUNG Gear Fit - negro - Reloj conectado',
+    precio: 199.00,
+    imagen: 'images/gear.jpg',
+    stock: 34,
+}];
+
